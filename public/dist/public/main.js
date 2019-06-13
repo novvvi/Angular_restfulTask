27b6217d24d02ae70653c8c322272a0cecb82a51
NOVI <novi@NOVIs-MacBook-Pro.local>
2019-06-13 17:54:45 -0500

    updated

--- public/dist/public/main.js
+++ public/dist/public/main.js
@@ -175,6 +175,7 @@ let HttpService = class HttpService {
     constructor(_http) {
         this._http = _http;
         this.getTasks();
+        this.getPokemon();
     }
     getTasks() {
         // our http response is an Observable, store it in a variable
@@ -182,6 +183,23 @@ let HttpService = class HttpService {
         // subscribe to the Observable and provide the code we would like to do with our data from the response
         tempObservable.subscribe(data => console.log("Got our tasks!", data));
     }
+    getPokemon() {
+        let mew = this._http.get('https://pokeapi.co/api/v2/pokemon/mew/');
+        mew.subscribe(data => {
+            console.log("mew weight:", data.weight);
+            var typeURL = data.types[0].type.url;
+            var abilitiesURL = data.abilities[0].ability.url;
+            let typethis = this._http.get(typeURL);
+            typethis.subscribe(data1 => {
+                console.log(data1);
+            });
+            let pokemon = this._http.get(abilitiesURL);
+            pokemon.subscribe(data => {
+                console.log(`There are ${data["pokemon"].length} pokemon has the same ability as mew`);
+            });
+        });
+    }
+    ;
 };
 HttpService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
     Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
--- public/src/app/http.service.ts
+++ public/src/app/http.service.ts
@@ -6,13 +6,33 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HttpService {
 
+
   constructor(private _http: HttpClient){
     this.getTasks();
+    this.getPokemon();
   }
   getTasks(){
     // our http response is an Observable, store it in a variable
     let tempObservable = this._http.get('/task');
     // subscribe to the Observable and provide the code we would like to do with our data from the response
     tempObservable.subscribe(data => console.log("Got our tasks!", data));
- }
+  }
+  getPokemon(){
+    let mew = this._http.get('https://pokeapi.co/api/v2/pokemon/mew/');
+    mew.subscribe(data => {
+      console.log("mew weight:", data.weight)
+      var typeURL = data.types[0].type.url;
+      var abilitiesURL = data.abilities[0].ability.url
+      
+      let typethis = this._http.get(typeURL);
+      typethis.subscribe(data1 => {
+        console.log(data1);
+      });
+      
+      let pokemon = this._http.get(abilitiesURL);
+      pokemon.subscribe(data => {
+        console.log(`There are ${data["pokemon"].length} pokemon has the same ability as mew`);
+      });
+    });
+  };
 }
